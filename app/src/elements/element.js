2fd7fe8aa8d6d1cb37e944e789a6e71a574a029c
Ben Nolan <ben@nolanconsul.com>
2015-03-10 16:12:30 +1300

    Add diff method to element.js.

--- app/src/elements/element.js
+++ app/src/elements/element.js
@@ -1,49 +1,63 @@
-(function() {
-  var Element, URI, Utils;
-
+var URI = require("uri-js"),
   Utils = require("../utils");
 
-  URI = require("uri-js");
+function Element(connector, el) {
+  this.connector = connector;
+  this.el = el;
+}
 
-  Element = (function() {
-    function Element(connector, el) {
-      this.connector = connector;
-      this.el = el;
-      true;
+Element.prototype.resolveURI = function(url) {
+  return URI.resolve(this.connector.uri, url);
+};
+
+Element.prototype.getPosition = function() {
+  if (this.el.attr("position")) {
+    return Utils.parseVector(el.attr("position"));
+  } else {
+    throw "No position specified for <" + this.el[0].nodeName + " />";
+  }
+};
+
+Element.prototype.getQuaternion = function() {
+  if (this.el.attr("rotation")) {
+    return new THREE.Quaternion().setFromEuler(Utils.parseEuler(el.attr("rotation")));
+  } else {
+    return new THREE.Quaternion();
+  }
+};
+
+Element.prototype.getScale = function() {
+  if (this.el.attr("scale")) {
+    return Utils.parseVector(this.el.attr("scale"));
+  } else {
+    return new THREE.Vector3(1, 1, 1);
+  }
+};
+
+// Has this element changed other than position and rotation? A and B should be
+// raw dom / xml nodes, not jQuery wrapped nodes.
+Element.substantialDifference = function(a, b){
+  if(a.attributes.length != b.attributes.length){
+    return true;
+  }
+
+  var i;
+
+  for(i=0;i<a.attributes.length;i++){
+    var name = a.attributes[i].name;
+
+    if(name === "position"){
+      continue;
     }
+    if(name === "rotation"){
+      continue;      
+    }
+    if(a.getAttribute(name) != b.getAttribute(name)){
+      return true;
+    }
+  }
 
-    Element.prototype.resolveURI = function(url) {
-      return URI.resolve(this.connector.uri, url);
-    };
+  return false;
+}
 
-    Element.prototype.getPosition = function() {
-      if (this.el.attr("position")) {
-        return Utils.parseVector(el.attr("position"));
-      } else {
-        throw "No position specified for <" + this.el[0].nodeName + " />";
-      }
-    };
-
-    Element.prototype.getQuaternion = function() {
-      if (this.el.attr("rotation")) {
-        return new THREE.Quaternion().setFromEuler(Utils.parseEuler(el.attr("rotation")));
-      } else {
-        return new THREE.Quaternion();
-      }
-    };
-
-    Element.prototype.getScale = function() {
-      if (this.el.attr("scale")) {
-        return Utils.parseVector(this.el.attr("scale"));
-      } else {
-        return new THREE.Vector3(1, 1, 1);
-      }
-    };
-
-    return Element;
-
-  })();
-
-  module.exports = Element;
-
-}).call(this);
+module.exports = Element;

aa9f2fc36b0fb58dd227e40e5d237d406f827de9
Ben Nolan <ben@nolanconsul.com>
2015-03-16 17:09:01 +1300

    Made a queue for billboards. Fixes #50. Updated html2canvas. Not sure if performance is better or worse.

--- app/src/elements/billboard.js
+++ app/src/elements/billboard.js
@@ -1,82 +1,122 @@
-(function() {
-  var Billboard, SIZE, Utils;
+var Utils = require("../utils");
 
-  Utils = require("../utils");
+// look of disapproval
+window.html2canvas = require("html2canvas");
 
-  Billboard = (function() {
-    function Billboard() {
-      true;
-    }
+function RenderQueue(){
+  this.queue = [];
+  this.processing = false;
+  this.interjobDelay = 25;
+}
 
-    return Billboard;
+RenderQueue.prototype.add = function(job){
+  this.queue.push(job);
+  
+  if(!this.processing){
+    this.nextJob();
+  }
+}
 
-  })();
+RenderQueue.prototype.nextJob = function(){
+  var job = this.queue.shift(),
+    self = this;
 
-  SIZE = 512;
+  if(!job || this.processing){
+    return;
+  }
 
-  Billboard.create = function(connector, el) {
-    var box, boxBody, boxShape, canvas, div, geometry, material, mesh, newScale, obj;
-    obj = new THREE.Object3D;
-    canvas = $("<canvas width='" + SIZE + "' height='" + SIZE + "' />")[0];
-    div = $("<div />").html(el.text()).css({
-      zIndex: 50,
-      position: 'absolute',
-      left: 0,
-      top: 0,
-      background: 'white',
-      width: SIZE,
-      height: SIZE,
-      padding: '10px',
-      border: '1px solid #ccc',
-      fontSize: '22px'
-    });
-    div.find("img").each((function(_this) {
-      return function(index, img) {
-        return img.src = URI.resolve(connector.uri, img.getAttribute("src"));
-      };
-    })(this));
-    div.appendTo('body');
-    geometry = new THREE.BoxGeometry(1, 1, 1);
-    material = new THREE.MeshLambertMaterial({
-      color: '#eeeeee',
-      ambient: '#eeeeee'
-    });
-    box = new THREE.Mesh(geometry, material);
-    material = new THREE.MeshLambertMaterial({
-      color: '#ffffff'
-    });
-    mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(1, 1), material);
-    mesh.position.setZ(0.52);
+  console.log("Starting job...");
+
+  this.processing = true;
+
+  job(function(){
+    setTimeout(function(){
+      self.processing = false;
+      self.nextJob();
+    }, self.interjobDelay);
+  });
+}
+
+var rQueue = new RenderQueue;
+
+var SIZE = 512;
+
+function Billboard() {
+}
+
+Billboard.create = function(connector, el) {
+  var box, boxBody, boxShape, canvas, div, geometry, material, mesh, newScale, obj;
+  obj = new THREE.Object3D;
+  canvas = $("<canvas width='" + SIZE + "' height='" + SIZE + "' />")[0];
+
+  div = $("<div />").html(el.text()).css({
+    zIndex: 50,
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    background: 'white',
+    width: SIZE,
+    height: SIZE,
+    padding: '10px',
+    border: '1px solid #ccc',
+    fontSize: '22px'
+  });
+  div.find("img").each((function(_this) {
+    return function(index, img) {
+      return img.src = URI.resolve(connector.uri, img.getAttribute("src"));
+    };
+  })(this));
+  div.appendTo('body');
+  geometry = new THREE.BoxGeometry(1, 1, 1);
+  material = new THREE.MeshLambertMaterial({
+    color: '#eeeeee',
+    ambient: '#eeeeee'
+  });
+  box = new THREE.Mesh(geometry, material);
+  material = new THREE.MeshLambertMaterial({
+    color: '#ffffff'
+  });
+  mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(1, 1), material);
+  mesh.position.setZ(0.52);
+
+  obj.add(box);
+  obj.add(mesh);
+  newScale = el.attr("scale") ? Utils.parseVector(el.attr("scale")) : new THREE.Vector3(2, 2, 0.5);
+  obj.scale.copy(newScale);
+  boxShape = new CANNON.Box(new CANNON.Vec3().copy(newScale.multiplyScalar(0.5)));
+  boxBody = new CANNON.Body({
+    mass: 0
+  });
+  boxBody.addShape(boxShape);
+
+  obj.body = boxBody;
+
+  var self = this;
+
+  rQueue.add(function(finished){
     html2canvas(div[0], {
       useCORS: true,
-      onrendered: (function(_this) {
-        return function(canvas) {
-          var texture;
-          texture = new THREE.Texture(canvas);
-          texture.needsUpdate = true;
-          material = new THREE.MeshBasicMaterial({
-            map: texture,
-            side: THREE.DoubleSide
-          });
-          material.transparent = false;
-          mesh.material = material;
-          return div.remove();
-        };
-      })(this)
-    });
-    obj.add(box);
-    obj.add(mesh);
-    newScale = el.attr("scale") ? Utils.parseVector(el.attr("scale")) : new THREE.Vector3(2, 2, 0.5);
-    obj.scale.copy(newScale);
-    boxShape = new CANNON.Box(new CANNON.Vec3().copy(newScale.multiplyScalar(0.5)));
-    boxBody = new CANNON.Body({
-      mass: 0
-    });
-    boxBody.addShape(boxShape);
-    obj.body = boxBody;
-    return obj;
-  };
+      letterRendering : false
+    }).then(function(canvas){
+      var texture;
+      texture = new THREE.Texture(canvas);
+      texture.needsUpdate = true;
 
-  module.exports = Billboard;
+      material = new THREE.MeshBasicMaterial({
+        map: texture,
+        side: THREE.DoubleSide
+      });
 
-}).call(this);
+      material.transparent = false;
+      mesh.material = material;
+
+      div.remove();
+
+      finished();
+    });
+  });
+
+  return obj;
+};
+
+module.exports = Billboard;

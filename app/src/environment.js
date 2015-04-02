f064d94893ee6c444b4809eff7d88f7494328dc3
Ben Nolan <ben@nolanconsul.com>
2015-04-03 11:11:45 +1300

    Semistandard.

--- app/src/environment.js
+++ app/src/environment.js
@@ -1,33 +1,31 @@
-'use strict';
-
-function Environment(){
+function Environment () {
 }
 
-Environment.prototype.isMobile = function(){
+Environment.prototype.isMobile = function () {
   return !!(/Android|iPhone|iPad|iPod|IEMobile/i.test(navigator.userAgent));
-}
+};
 
-Environment.prototype.physicsHertz = function(){
+Environment.prototype.physicsHertz = function () {
   return 60.0;
-}
+};
 
-Environment.prototype.isDebug = function(){
+Environment.prototype.isDebug = function () {
   return false;
-}
+};
 
 // Render at a reduced framerate (are you developing the client on the plane to sydney? you want this)
-Environment.prototype.isLowPowerMode = function(){
+Environment.prototype.isLowPowerMode = function () {
   return false;
-}
+};
 
 // Feature flag
-Environment.prototype.isEditingEnabled = function(){
+Environment.prototype.isEditingEnabled = function () {
   return false;
-}
+};
 
 // unpublish after 90 seconds of inactivity
-Environment.prototype.unpublishTimeout = function(){
+Environment.prototype.unpublishTimeout = function () {
   return 90 * 1000;
-}
+};
 
-module.exports = new Environment;
+module.exports = new Environment();
